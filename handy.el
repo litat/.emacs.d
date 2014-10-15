fa574191672ae5ac20f7d32237f0a73170e75abf
Lita Chang <fireice47@gmail.com>
2014-10-15 13:05:37 +0800

    Add a file handy.el for some convient features.And install much more packages.

---
+++ handy.el
@@ -0,0 +1,16 @@
+;; 
+;; Some handy features that built-in emacs.
+;; 
+
+(whitespace-mode)
+(glasses-mode)
+(subword-mode)
+(icomplete-mode)
+(show-paren-mode)
+(electric-pair-mode)
+(delete-trailing-whitespace)
+(narrow-to-defun)
+
+;; Features that needs parameters.
+(ffap)
+(flush-lines)
--- init.el
+++ init.el
@@ -4,17 +4,40 @@
 
 ;; display
 (modify-all-frames-parameters '((menu-bar-lines . 0)))
+(set-face-background 'mode-line nil)
+(set-face-background 'mode-line-inactive nil)
+
+;; ediff
+(setq ediff-split-window-function 'split-window-horizontally)
 
 ;;
 ;; packages
 ;;
 
-;; ediff
-(setq ediff-split-window-function 'split-window-horizontally)
+;; installed packages:
+;;   ace-jump-mode
+;;   auto-complete
+;;   csv-mode
+;;   js2-mode
+;;   markdown-mode
+;;   multiple-cursors
+;;   popup
+;;   web-mode
+;;   yasnippet
+
+;; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+;; multiple-cursors
+(global-set-key (kbd "C-c l") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 ;; other elisp files to load
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (autoload 'css-syntax-color-hex "css-syntax-color-hex" nil t)
+
